bfa82b5e901718ef59cf40ddfe25764944965bc7
Raphael Heer <raphael.heer@hslu.ch>
2017-06-08 14:02:07 +0200

    Change to Ilias 5.2
    Changed the min and max version of Ilias for this plugin in plugin.php

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'hsluobjdef';
 $version = '1.0.0';
-$ilias_min_version = '4.4';
-$ilias_max_version = '5.1.999';
+$ilias_min_version = '5.2';
+$ilias_max_version = '5.2.999';
 $responsible = 'Simon Moor';
 $responsible_mail = 'simon.moor@hslu.ch';
 ?>
