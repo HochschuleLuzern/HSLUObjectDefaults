2f17afa99741a8b97dd961606ae383eed70a1f45
Raphael Heer <raphael.heer@hslu.ch>
2020-04-15 14:35:53 +0200

    Starting update to 6.0

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'hsluobjdef';
 $version = '1.0.3';
-$ilias_min_version = '5.3';
-$ilias_max_version = '5.4.999';
+$ilias_min_version = '6.0';
+$ilias_max_version = '6.999';
 $responsible = 'Stephan Winiker';
 $responsible_mail = 'stephan.winiker@hslu.ch';
 $responsible = 'Raphael Heer';
