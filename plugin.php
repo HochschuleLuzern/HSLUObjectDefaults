29f50839d6c84324a25ceb5f517155992f115632
Stephan Winiker <stephan.winiker@hslu.ch>
2021-05-27 12:02:16 +0200

    Start Update to ILIAS 7

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'hsluobjdef';
 $version = '1.0.3';
-$ilias_min_version = '6.0';
-$ilias_max_version = '6.999';
+$ilias_min_version = '7.0';
+$ilias_max_version = '7.999';
 $responsible = 'Stephan Winiker';
 $responsible_mail = 'stephan.winiker@hslu.ch';
 $responsible = 'Raphael Heer';
