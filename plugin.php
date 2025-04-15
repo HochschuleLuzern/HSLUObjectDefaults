b77598e7c5c6a841a6597053a57ecb52f5cc9c49
msalterhslu <mark.salter@hslu.ch>
2025-04-15 15:17:12 +0200

    Compatibility with ILIAS 9

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'hsluobjdef';
-$version = '1.1.2';
-$ilias_min_version = '7.0';
-$ilias_max_version = '8.999';
+$version = '1.1.3';
+$ilias_min_version = '8.0';
+$ilias_max_version = '9.999';
 $responsible = 'Mark Salter';
 $responsible_mail = 'mark.salter@hslu.ch';
 $responsible = 'Raphael Heer';
